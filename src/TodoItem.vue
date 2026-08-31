01a0c553e0cd767424ca33ceca9e5e86b3d3ba0d
Yen Heab <yenheab@gmail.com>
2026-08-31 15:51:59 +0700

    not change

--- src/TodoItem.vue
+++ src/TodoItem.vue
@@ -10,7 +10,7 @@ const emit = defineEmits(['toggle', 'delete'])
 <template>
   <div class="task" :class="{ 'is-completed': completed }">
 
-    <!-- Checkbox រង្វង់មូល (មានសញ្ញាគ្រីសពេល Completed) -->
+    
     <button
       class="checkbox"
       :class="{ checked: completed }"
@@ -19,12 +19,12 @@ const emit = defineEmits(['toggle', 'delete'])
       <span v-if="completed" class="checkmark">✓</span>
     </button>
 
-    <!-- អក្សរ Task Name -->
+    
     <span class="task-text">
       {{ title }}
     </span>
 
-    <!-- ប៊ូតុងរូបធុងសំរាមសម្រាប់លុប -->
+    
     <button class="delete-btn" @click="emit('delete')">
       <svg xmlns="http://w3.org" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M3 6h18"></path><path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6"></path><path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2"></path></svg>
     </button>
@@ -42,10 +42,10 @@ const emit = defineEmits(['toggle', 'delete'])
 }
 
 .task:last-child {
-  border-bottom: none; /* លុបបន្ទាត់ក្រោមបង្អស់ចេញ */
+  border-bottom: none; 
 }
 
-/* រៀបចំស្ទីល Checkbox រង្វង់មូល */
+
 .checkbox {
   width: 22px;
   height: 22px;
@@ -61,7 +61,7 @@ const emit = defineEmits(['toggle', 'delete'])
   transition: all 0.2s;
 }
 
-/* ស្ទីលនៅពេលកិច្ចការធ្វើរួចរាល់ (Completed) */
+
 .task.is-completed .checkbox {
   border-color: #df8c25;
   background-color: transparent;
@@ -80,13 +80,13 @@ const emit = defineEmits(['toggle', 'delete'])
   font-weight: 500;
 }
 
-/* ស្ទីលអក្សរពេលកិច្ចការធ្វើរួចរាល់ (គូសចំកណ្តាល និងពណ៌ស្រអាប់) */
+
 .task.is-completed .task-text {
   color: #c4b9ad;
   text-decoration: line-through;
 }
 
-/* ប៊ូតុងលុបរូបធុងសំរាម */
+
 .delete-btn {
   border: none;
   background: transparent;
